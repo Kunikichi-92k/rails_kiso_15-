928f01c596418599df901aa7dda4756fef5cadc8
Kunikichi-92k <access92k@gmail.com>
2021-10-01 18:22:33 +0900

    05_i18n

--- Gemfile
+++ Gemfile
@@ -67,3 +67,4 @@ end
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 gem 'sorcery'
 gem 'pry-byebug'
+gem 'rails-i18n'
--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module RunteqNormal
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
   end
 end
 
