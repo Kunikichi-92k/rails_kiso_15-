dc508e4957ab6916f7d337ad2618704d4c748d30
Kunikichi-92k <access92k@gmail.com>
2021-10-05 22:57:39 +0900

    08_board_index

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,13 +1,12 @@
 class UserSessionsController < ApplicationController
-  
-  def new
-  end
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new; end
 
   def create
     @user = login(params[:email], params[:password])
-
     if @user
-      redirect_back_or_to root_path, success: t('.success')
+      redirect_back_or_to boards_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
       render :new
--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,9 @@ module RunteqNormal
 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
 
