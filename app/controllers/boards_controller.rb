f5fd9b9c665dc35403ecaac77271379f9b04e2da
Kunikichi-92k <access92k@gmail.com>
2021-10-16 17:37:57 +0900

    14_board_edit_delete

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :find_board, only: [:edit, :update, :destroy]
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
   end
@@ -23,9 +24,29 @@ class BoardsController < ApplicationController
     @comments = @board.comments.includes(:user).order(created_at: :desc)
   end
 
+  def edit; end
+
+  def update
+    if @board.update(board_params)
+      redirect_to @board, success: t('defaults.message.updated', item: Board.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: Board.model_name.human)
+      render :edit
+    end
+  end
+
+  def destroy
+    @board.destroy!
+    redirect_to boards_path, success: t('defaults.message.deleted', item: Board.model_name.human)
+  end
+
   private
 
   def board_params
     params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
+  end
+
+  def find_board
+    @board = current_user.boards.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: [:new, :create]
-  resources :boards, only: %i[index new create show] do
+  resources :boards do
     resources :comments, only: %i[create], shallow: true
   end
 end
